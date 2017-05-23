5e3c1082df8dc0884412075a83d74e08054df64f
lisyahantika <lisyahantika96@gmail.com>
2017-05-23 13:03:04 +0700

    Add files via upload

---
+++ jsondb.php
@@ -0,0 +1,15 @@
+<?php 
+ $db_host = "localhost";
+ $db_user = "root";
+ $db_pass = "";
+ $db_name = "json_db";
+ $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+ $sql = "Select * from kategori";
+ $query = mysqli_query($koneksi, $sql);
+ $rows=array();
+ while($data = mysqli_fetch_assoc($query)){
+ $rows[] = $data;
+ $rows[] = "<br>";
+ }
+ echo json_encode($rows);
+?>
